a6f4061973c0649bd6d3eaa677f67fd496f603c7
tatyana-kuznecova <tatiana.kuzart@yahoo.com>
2025-01-10 14:22:15 +0300

    Fix

--- src/main.js
+++ src/main.js
@@ -1,58 +1,14 @@
-window.addEventListener("resize", () => {
-    swiperCard()
-    hideBrands()
-});
-document.addEventListener("DOMContentLoaded", () => {
-    swiperCard();
-    hideBrands();
-});
-
-const brandItems = document.querySelector(".brand__items").getElementsByClassName('brand__item');
-const showMore = document.querySelector(".brand__show-more");
-let hideBrand = true;
-
-document.querySelector(".brand__show-more").onclick = () => {
-    if (hideBrand) {
-        for (let i = 0; i < brandItems.length; i++) {
-            brandItems[i].style.display = 'block';
-        }
-        hideBrand = false;
-        showMore.textContent = 'Скрыть';
-        showMore.style.backgroundImage = "url(img/hide.svg)";
-    } else {
-        hideBrand = true;
-        hideBrands();
-        showMore.textContent = 'Показать все';
-        showMore.style.backgroundImage = "url(img/show-more.svg)";
-
-    }
-    return false
-}
-
-
-function hideBrands() {
-    for (let i = 0; i < brandItems.length; i++) {
-        brandItems[i].style.display = 'block';
-        if (window.innerWidth >= 768 && window.innerWidth < 1120 && i > 5) {
-            brandItems[i].style.display = 'none';
-        }
-        if (window.innerWidth >= 1120 && i > 7) {
-            brandItems[i].style.display = 'none';
-        }
-    }
-    hideBrand = true;
-
-}
-
-let swiper;
 let init = false;
+let swiper;
+
 function swiperCard() {
-    if (window.innerWidth < 768) {
+    if (window.innerWidth <= 767) {
         if (!init) {
             init = true;
-            swiper = new Swiper(".swiper", {
+
+            let swiper = new Swiper(".swiper", {
                 direction: 'horizontal',
-                spaceBetween: 60,
+                spaceBetween: 10,
                 slidesPerView: 1.5,
                 pagination: {
                     el: '.swiper-pagination',
@@ -65,3 +21,43 @@ function swiperCard() {
         init = false;
     }
 }
+
+swiperCard();
+window.addEventListener("resize", swiperCard);
+
+const content = document.querySelectorAll('.swiper-slide');
+const buttonShow = document.querySelector('.brand__more_show');
+const buttonHide = document.querySelector('.brand__more_hide');
+
+
+buttonShow.addEventListener("click", event => {
+    event.preventDefault();
+
+    buttonHide.classList.toggle('hidden');
+    buttonShow.classList.toggle('hidden');
+
+
+    content.forEach((item) => {
+        item.style.display = 'block';
+    })
+})
+
+buttonHide.addEventListener("click", event => {
+    event.preventDefault();
+    buttonHide.classList.toggle('hidden');
+    buttonShow.classList.toggle('hidden');
+
+    content.forEach((item, index) => {
+        if (window.innerWidth > 1119 && index > 7) {
+            item.style.display = 'none';
+        }
+        if (window.innerWidth > 767 && window.innerWidth < 1120 && index > 5) {
+            item.style.display = 'none';
+        }
+    })
+})
+
+
+
+
+
